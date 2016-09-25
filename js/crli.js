4d9f2641d916dcf38fa912b17be17a6609ecf932
crli <695922129@qq.com>
2016-09-25 12:58:14 +0800

    s

--- js/crli.js
+++ js/crli.js
@@ -129,7 +129,6 @@ if (window.SIZE == 'S') {
 window.applicationCache.addEventListener('updateready',function(e){
     if(window.applicationCache.status == window.applicationCache.UPDATEREADY){
         window.applicationCache.swapCache();       
-        window.location.reload()
     }
 },false)   
 
